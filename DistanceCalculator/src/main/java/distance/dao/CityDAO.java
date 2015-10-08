079b89d25e0a3bd5758f286c11e867b4c1d655d1
Vitor Mussatto <mussatto@gmail.com>
2015-10-07 22:02:06 -0300

    dao

--- DistanceCalculator/src/main/java/distance/dao/CityDAO.java
+++ DistanceCalculator/src/main/java/distance/dao/CityDAO.java
@@ -1,11 +1,39 @@
 package distance.dao;
 
+import distance.calc.City;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CityDAO {
 
-    public void connect(){
+    private Connection connect = null;
+
+    public void connect() throws ClassNotFoundException, SQLException {
         String host = "192.168.99.100";
         String user = "root";
         String pass = "distance";
+        String database = "distance";
         String port = "3306";
+        String connString = "jdbc:mysql://"+host+":"+port+"/"+database+"?user="+user+"&password="+pass;
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        connect = DriverManager.getConnection(connString);
+
+        Statement statement = connect.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("select * from CITY");
+        List<City> cities = new ArrayList<City>();
+        while (resultSet.next()){
+            String name = resultSet.getString("NAME");
+            Double latitude = resultSet.getDouble("LATITUDE");
+            Double longitude = resultSet.getDouble("LONGITUDE");
+
+            cities.add(City.create(latitude, longitude, name));
+        }
+
+        System.out.println(cities);
     }
 }
