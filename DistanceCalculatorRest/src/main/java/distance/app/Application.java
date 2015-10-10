b9c71924f50275e18e9685b32c7976113d7f8b5e
Vitor Mussatto <mussatto@gmail.com>
2015-10-10 10:24:55 -0300

    deployable in tomcat

--- DistanceCalculatorRest/src/main/java/distance/app/Application.java
+++ DistanceCalculatorRest/src/main/java/distance/app/Application.java
@@ -3,6 +3,8 @@ package distance.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -11,7 +13,12 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
